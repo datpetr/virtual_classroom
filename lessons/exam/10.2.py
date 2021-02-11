1d6ba6cb5bde1e74169f3b0534fc8bd26d4835ae
datpetr <57681066+datpetr@users.noreply.github.com>
2021-02-11 22:01:59 +0300

    Delete lessons directory

--- lessons/exam/10.2.py
+++
@@ -1,4 +0,0 @@
-for i in range(1, 1000):
-    if (9 * i + 4) % 10 == 7:
-        print(i)
-        break
--- lessons/exam/10.py
+++
@@ -1,4 +0,0 @@
-for i in range(1, 1000):
-    if ((6 * i + 2) % 7) == 3:
-        print(i)
-        break
